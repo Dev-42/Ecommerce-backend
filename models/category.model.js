000bdc52eea5da72d5daddcb9a0c396c9d724b37
server_42 <devbhattacharya42@gmail.com>
2024-09-01 22:38:10 +0530

    One to many relationship established between items and category model

--- models/category.model.js
+++ models/category.model.js
@@ -12,6 +12,12 @@ const categorySchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    items: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Item",
+      },
+    ],
   },
   {
     timestamps: true,
---
+++ models/items.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const itemSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+const ItemModel = mongoose.model("Item", itemSchema);
+module.exports = { ItemModel };
